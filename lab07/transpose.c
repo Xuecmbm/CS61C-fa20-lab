151677e840160d141415e4bb525a14587b6868ae
baomihua <1124653720@qq.com>
2024-08-02 15:31:12 +0800

    Lab 7: Finish experiment - End of Lab 7

--- lab07/transpose.c
+++ lab07/transpose.c
@@ -13,4 +13,24 @@ void transpose_naive(int n, int blocksize, int *dst, int *src) {
  * multiple of the block size. */
 void transpose_blocking(int n, int blocksize, int *dst, int *src) {
     // YOUR CODE HERE
+    int i, j;
+    for(i = 0; i < n / blocksize; i++){
+        for(j = 0; j < n / blocksize; j++){
+            for(int x = 0; x < blocksize; x++){
+                for(int y = 0; y < blocksize; y++){
+                    dst[(y + j * blocksize) + (x + i * blocksize) * n] = src[(x + i * blocksize) + (y + j * blocksize) * n];
+                }
+            }
+        }
+        for(int x = 0; x < blocksize; x++){
+            for(int y = j * blocksize; y < n; y++){
+                dst[y + (x + i * blocksize) * n] = src[(x + i * blocksize) + y * n];
+            }
+        }
+    }
+    for(int x = i * blocksize; x < n; x++){
+        for(int y = 0; y < n; y++){
+            dst[y + x * n] = src[x + y * n];
+        }
+    }
 }
